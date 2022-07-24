664b217e65b6ade7db508c1c1b0bc286b16dd90c
Matias-Desktop <matias.kronlund@outlook.com>
2022-07-25 01:02:39 +0300

    Added Popup

--- src/components/main/popup.js
+++ src/components/main/popup.js
@@ -4,18 +4,38 @@ import { AiFillCloseCircle } from "react-icons/ai";
 
 function Popup({ recipe, closeFunction }) {
     let hide = true;
-    if (Object.keys(recipe).length !== 0) hide = false;
-    return (
-        <div className="rpopup" hidden={hide}>
-            <div className="recipeCont">
-                <AiFillCloseCircle
-                    className="close"
-                    onClick={closeFunction}
-                ></AiFillCloseCircle>
-                <div className="recipe"></div>
+    if (Object.keys(recipe).length !== 0) {
+        return (
+            <div className="rpopup">
+                <div className="recipeCont">
+                    <AiFillCloseCircle
+                        className="close"
+                        onClick={closeFunction}
+                    ></AiFillCloseCircle>
+                    <div className="recipe">
+                        <div>
+                            <h3>{recipe.name}</h3>
+
+                            <ol>
+                                {recipe.steps.map((step, index) => (
+                                    <li key={index}>{step}</li>
+                                ))}
+                            </ol>
+                        </div>
+                        <div>
+                            <img src={recipe.image} alt="" />
+                            <h4>Igredients</h4>
+                            <ul>
+                                {recipe.ingredients.map((ing, index) => (
+                                    <li key={index}>{ing}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Popup;
